ddf767da21161894e88ce24b8c178279ca9b1a20
Trish Ness <trishnss@gmail.com>
2020-08-15 16:17:37 -0700

    repush

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import About from "./pages/About";
-import Portfolio from "./pages/Portfolio"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Portfolio from "./pages/Portfolio";
 import Nav from "./components/Nav";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 function App() {
   return (
